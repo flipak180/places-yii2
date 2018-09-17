ebeced04dbe4f107c39cf26684286b868e5f8af3
skav <flipak180@mail.ru>
2018-09-17 23:34:04 +0300

    Test

---
+++ console/migrations/m180917_202943_places.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180917_202943_places
+ */
+class m180917_202943_places extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180917_202943_places cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180917_202943_places cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
